8aba54a0fe5935fec8d87f9f4bf621540a869e2e
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-02 14:52:07 +0530

    written the logic in MathematicalCalculator for divide

--- src/main/java/com/jap/calculator/MathematicalCalculator.java
+++ src/main/java/com/jap/calculator/MathematicalCalculator.java
@@ -2,37 +2,32 @@ package com.jap.calculator;
 
 public class MathematicalCalculator {
 
-	public int add(int num1, int num2)
-	{
-		return  num1 + num2;
-	}
-	public int subtract(int num1, int num2)
-	{	
-		return  num1 - num2;
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
 
-	}
-	public int multiply(int num1, int num2)
-	{	
-		return num1 * num2;
-	}
-	/*
-	public int divide(int num1, int num2) {
-		//write the code and handle the ArithmeticException
-		int result = 0;
-		try {
-			result = num1 / num2;
-		} catch (ArithmeticException exception) {
-			System.out.println(exception.getMessage());
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
 
-		}
-		return result;
-	}
+    }
 
-	 */
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
 
+    public int divide(int num1, int num2) {
+        //write the code and handle the ArithmeticException
+        int result = 0;
+        try {
+            result = num1 / num2;
+        } catch (ArithmeticException exception) {
+            System.out.println(exception.getMessage());
 
-	public int modulo(int num1, int num2)
-	{
-		return num1 % num2;
-	}
+        }
+        return result;
+    }
+
+    public int modulo(int num1, int num2) {
+        return num1 % num2;
+    }
 }
